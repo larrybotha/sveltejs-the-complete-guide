c4963aa3f9462908eacf003fdad86370d03dae44
Larry Botha <larry@fixate.it>
2020-02-27 16:41:10 -0500

    add lesson files

---
+++ section-02/16-using-multiple-components/src/contact-card.svelte
@@ -0,0 +1,16 @@
+<style>
+
+</style>
+
+<div>
+  <header>
+    <img src="" alt="" />
+
+    <div>
+      <h1>User name</h1>
+      <h2>Job title</h2>
+    </div>
+  </header>
+
+  <p>A short description</p>
+</div>
