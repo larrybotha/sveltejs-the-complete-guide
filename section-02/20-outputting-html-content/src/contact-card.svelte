59996a9d5d5ce40affdc3eaad1f89e0e0d274a6a
Larry Botha <larry@fixate.it>
2020-02-27 17:54:53 -0500

    add example using html content

---
+++ section-02/20-outputting-html-content/src/contact-card.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let userName;
+  export let jobTitle;
+  export let imgUrl;
+  export let imgAlt;
+  export let description;
+</script>
+
+<style>
+
+</style>
+
+<div>
+  <header>
+    <img src={imgUrl} alt={imgAlt} />
+
+    <div>
+      <h1>{userName}</h1>
+      <h2>{jobTitle}</h2>
+    </div>
+  </header>
+
+  <p>{description}</p>
+  <p>
+    {@html description}
+  </p>
+</div>
