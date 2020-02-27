ea9b128d0baf748e07538311d3397838b6011448
Larry Botha <larry@fixate.it>
2020-02-27 15:52:07 -0500

    add example with 2-way binding

--- section-02/15-two-way-binding-shortcut/src/App.svelte
+++ section-02/15-two-way-binding-shortcut/src/App.svelte
@@ -7,10 +7,6 @@
   function incrementAge() {
     age += 1;
   }
-
-  function handleInput({ target }) {
-    name = target.value;
-  }
 </script>
 
 <main>
@@ -18,5 +14,15 @@
 
   <button on:click={incrementAge}>inc age</button>
 
-  <input type="text" value={name} on:input={handleInput} />
+  <!--
+    Instead of creating 2 explicit bindings on input and value, one can benefit
+    from a shortcut that Svelte makes available through its `bind` syntax
+
+    `bind` can be used with most HTML attributes, and sets up 2-way binding,
+    eliminating the need for manually creating bindings
+
+    Beware of overusing this 2-way binding, as the more explicit unidirectional
+    bindings can be useful for making code easier to read
+  -->
+  <input type="text" bind:value={name} />
 </main>
