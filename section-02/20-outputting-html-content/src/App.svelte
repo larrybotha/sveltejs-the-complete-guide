1f8dfad7a519bd92e7e04ee93635e32ee703c634
Larry Botha <larry@fixate.it>
2020-03-03 22:25:46 -0500

    add XSS example

--- section-02/20-outputting-html-content/src/App.svelte
+++ section-02/20-outputting-html-content/src/App.svelte
@@ -4,7 +4,14 @@
   let age = 30;
   let name = "Joe";
   let jobTitle = "Awesome show, great job";
-  let description = "<strong>hey!</strong> that is bold";
+  let description = `
+      <strong>hey!</strong> that is bold, and here's an XSS attack in an img
+      <img
+        src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+        onload="alert('You were hacked!');"
+      >
+
+  `;
   let imgUrl = "http://via.placeholder.com/150";
   let imgAlt = "img alt";
 
