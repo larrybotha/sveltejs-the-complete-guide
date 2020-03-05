9aa6a5b0487ba09add814f388893f988d893a5cd
Larry Botha <larry@fixate.it>
2020-03-04 23:47:35 -0500

    add example and notes

---
+++ section-03/29-each-else-and-extracting-the-index/src/App.svelte
@@ -0,0 +1,78 @@
+<script>
+  import ContactCard from "./contact-card.svelte";
+
+  let name = "Max";
+  let title = "";
+  let image = "";
+  let description = "";
+  let formState = "empty";
+
+  let createdContacts = [];
+
+  function addContact() {
+    if (
+      name.trim().length == 0 ||
+      title.trim().length == 0 ||
+      image.trim().length == 0 ||
+      description.trim().length == 0
+    ) {
+      formState = "invalid";
+      return;
+    }
+    createdContacts = [
+      ...createdContacts,
+      {
+        name: name,
+        jobTitle: title,
+        imageUrl: image,
+        desc: description
+      }
+    ];
+    formState = "done";
+  }
+</script>
+
+<style>
+  #form {
+    width: 30rem;
+    max-width: 100%;
+  }
+</style>
+
+<div id="form">
+  <div class="form-control">
+    <label for="userName">User Name</label>
+    <input type="text" bind:value={name} id="userName" />
+  </div>
+  <div class="form-control">
+    <label for="jobTitle">Job Title</label>
+    <input type="text" bind:value={title} id="jobTitle" />
+  </div>
+  <div class="form-control">
+    <label for="image">Image URL</label>
+    <input type="text" bind:value={image} id="image" />
+  </div>
+  <div class="form-control">
+    <label for="desc">Description</label>
+    <textarea rows="3" bind:value={description} id="desc" />
+  </div>
+</div>
+
+<button on:click={addContact}>Add Contact Card</button>
+
+{#if formState === 'invalid'}
+  <p>Invalid input.</p>
+{:else}
+  <p>Please enter some data and hit the button!</p>
+{/if}
+
+{#each createdContacts as contact, i}
+  <h2># {i + 1}</h2>
+  <ContactCard
+    userName={contact.name}
+    jobTitle={contact.jobTitle}
+    description={contact.desc}
+    userImage={contact.imageUrl} />
+{:else}
+  <p>Please start adding some contacts, we found none!</p>
+{/each}
--- section-03/README.md
+++ section-03/README.md
@@ -5,5 +5,15 @@
 <!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
 **Table of Contents**
 
+- [29. `each`, `else`, and extracting the index](#29-each-else-and-extracting-the-index)
+
 <!-- END doctoc generated TOC please keep comment here to allow auto update -->
 
+## 29. `each`, `else`, and extracting the index
+
+```bash
+$ npm run dev --prefix ./29-each-else-and-extracting-the-index
+```
+
+- Svelte's `each` block allows for an `else` statement which outputs content if
+    the array we're iterating over is empty
