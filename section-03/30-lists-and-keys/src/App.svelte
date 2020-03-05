d35b29df1efd0857b42a46459dfca22511449997
Larry Botha <larry@fixate.it>
2020-03-05 00:11:22 -0500

    add examplse and notes

---
+++ section-03/30-lists-and-keys/src/App.svelte
@@ -0,0 +1,93 @@
+<script>
+  import ContactCard from "./contact-card.svelte";
+
+  let name = "Max";
+  let title = "";
+  let image = "";
+  let description = "";
+  let formState = "empty";
+
+  let createdContacts = [];
+
+  function addContact() {
+    if (
+      name.trim().length == 0 ||
+      title.trim().length == 0 ||
+      image.trim().length == 0 ||
+      description.trim().length == 0
+    ) {
+      formState = "invalid";
+      return;
+    }
+    createdContacts = [
+      ...createdContacts,
+      {
+        id: Math.random(),
+        name: `${name} ${createdContacts.length}`,
+        jobTitle: title,
+        imageUrl: image,
+        desc: description
+      }
+    ];
+    formState = "done";
+  }
+
+  function deleteFirst() {
+    createdContacts = createdContacts.slice(-1);
+  }
+
+  function deleteLast() {
+    createdContacts = createdContacts.slice(0, -1);
+  }
+</script>
+
+<style>
+  #form {
+    width: 30rem;
+    max-width: 100%;
+  }
+</style>
+
+<div id="form">
+  <div class="form-control">
+    <label for="userName">User Name</label>
+    <input type="text" bind:value={name} id="userName" />
+  </div>
+  <div class="form-control">
+    <label for="jobTitle">Job Title</label>
+    <input type="text" bind:value={title} id="jobTitle" />
+  </div>
+  <div class="form-control">
+    <label for="image">Image URL</label>
+    <input type="text" bind:value={image} id="image" />
+  </div>
+  <div class="form-control">
+    <label for="desc">Description</label>
+    <textarea rows="3" bind:value={description} id="desc" />
+  </div>
+</div>
+
+<button on:click={addContact}>Add Contact Card</button>
+<button on:click={deleteFirst}>Delete first</button>
+<button on:click={deleteLast}>Delete last</button>
+
+{#if formState === 'invalid'}
+  <p>Invalid input.</p>
+{:else}
+  <p>Please enter some data and hit the button!</p>
+{/if}
+
+<!--
+  Without specifying a key on the loop, Svelte won't be able to properly update
+  the list
+-->
+{#each createdContacts as contact, i (contact.id)}
+  <h2># {i + 1}</h2>
+  <ContactCard
+    userName={contact.name}
+    jobTitle={contact.jobTitle}
+    description={contact.desc}
+    userImage={contact.imageUrl} />
+{:else}
+  <p>Please start adding some contacts, we found none!</p>
+{/each}
--- section-03/README.md
+++ section-03/README.md
@@ -6,6 +6,7 @@
 **Table of Contents**
 
 - [29. `each`, `else`, and extracting the index](#29-each-else-and-extracting-the-index)
+- [30. Lists and keys](#30-lists-and-keys)
 
 <!-- END doctoc generated TOC please keep comment here to allow auto update -->
 
@@ -17,3 +18,23 @@ $ npm run dev --prefix ./29-each-else-and-extracting-the-index
 
 - Svelte's `each` block allows for an `else` statement which outputs content if
     the array we're iterating over is empty
+
+## 30. Lists and keys
+
+```bash
+$ npm run dev --prefix ./30-lists-and-keys
+```
+
+- by default Svelte assumes items removed from lists are done so from the end of
+    the list. Because of this, if components in the list depend on internal data,
+    that internal data may become stale if items are removed from the list. Without
+    indicating to Svelte that items are unique in an array, Svelte can do little
+    to diff items
+- to address this, we provide the `each` block with a key that identifies each item
+    in the array uniquely
+
+    ```svelte
+    {#each items as item, i (item.id)}
+      // do stuff
+    {/each}
+    ```
