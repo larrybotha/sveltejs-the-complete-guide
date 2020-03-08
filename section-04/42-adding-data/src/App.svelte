50d137e89f8af32df2560f71524219a92e23800e
Larry Botha <larry@fixate.it>
2020-03-07 23:36:38 -0500

    add deps and files

---
+++ section-04/42-adding-data/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import faker from "faker";
+  import Header from "./UI/Header.svelte";
+
+  function createMeetup() {
+    return {
+      id: faker.random.uuid(),
+      title: faker.lorem.words(),
+      subtitle: faker.lorem.words(),
+      description: faker.lorem.words(),
+      imageUrl: faker.image.imageUrl(),
+      address: faker.address.streetName(),
+      contactEmail: faker.internet.email()
+    };
+  }
+
+  let meetups = Array.from({ length: 3 }).map(createMeetup);
+</script>
+
+<Header />
+
+<pre>{JSON.stringify(meetups, null, 2)}</pre>
