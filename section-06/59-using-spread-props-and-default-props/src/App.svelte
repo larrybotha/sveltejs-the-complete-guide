9c279538ac394501d87a193979d55980dcf9a706
Larry Botha <larry@fixate.it>
2020-03-08 23:08:42 -0500

    add notes and examples

---
+++ section-06/59-using-spread-props-and-default-props/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Product from "./Product.svelte";
+
+  const products = [
+    {
+      id: 1,
+      title: "product 1",
+      price: 5.0,
+      bestSeller: true
+    },
+    {
+      id: 2,
+      title: "product 2",
+      price: 5.0
+    }
+  ];
+</script>
+
+{#each products as product}
+  <Product
+    {...product}
+    on:click={ev => console.log(`clicked
+    "${ev.target.textContent}"`)}
+    on:add-to-cart={() => console.log('add to cart')}
+    on:delete={() => console.log('delete')} />
+{/each}
---
+++ section-06/59-using-spread-props-and-default-props/src/Product.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let title;
+  export let price;
+  // if a property is optional, set a default value
+  export let bestSeller = false;
+
+  const dispatch = createEventDispatcher();
+  const addToCart = () => {
+    dispatch("add-to-cart", ["cart data"]);
+  };
+</script>
+
+<article>
+  <h1>{title}</h1>
+  <div>{price}</div>
+
+  {#if bestSeller}
+    <div>Bestseller</div>
+  {/if}
+
+  <button on:click={addToCart}>add to cart, custom event</button>
+  <button on:click={() => dispatch('delete', ['custom data'])}>
+    delete, custom event
+  </button>
+</article>
