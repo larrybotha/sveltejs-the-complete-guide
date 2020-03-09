ef9a893c321cfbe3df77895119273c84062082f0
Larry Botha <larry@fixate.it>
2020-03-08 22:36:56 -0500

    remove comments

--- section-06/58-emitting=custom-events/src/Product.svelte
+++ section-06/58-emitting=custom-events/src/Product.svelte
@@ -2,18 +2,9 @@
   export let title;
 </script>
 
-<!--
-  event delegation works as expected, but only within a component
-
-  Events, by default, do not cross component boundaries
--->
-<article on:click={ev => console.log(`click event delegated: ${ev.target}`)}>
+<article></article>
   <h1>{title}</h1>
 
-  <!--
-    This event is forwarded to the component immediately importing this
-    component, because we don't define a handler
-  -->
   <button on:click>add to cart</button>
   <button>delete</button>
 </article>
