efdc29bce7891a990c4348465dc94240c3bc9932
Larry Botha <larry@fixate.it>
2020-03-21 23:14:24 -0500

    add files for 83

---
+++ section-08/83-understanding-custom-component-bindings/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import CustomInput from "./custom-input.svelte";
+  import Toggle from "./toggle.svelte";
+
+  let val = "Joe Soap";
+  let curr = 1;
+
+  $: console.log(val);
+  $: console.log(curr);
+</script>
+
+<CustomInput bind:val />
+
+<!--
+  We can bind to `current` inside the Toggle, and get values here
+
+  This is a two-way component binding, and is strongly discouraged, as it makes
+  debugging difficult
+-->
+<Toggle bind:current={curr} />
+
---
+++ section-08/83-understanding-custom-component-bindings/src/toggle.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let current = 0;
+</script>
+
+<button disabled={current === 0} on:click={() => (current = 0)}>0</button>
+
+<button disabled={current === 1} on:click={() => (current = 1)}>1</button>
