11c07b2bc85efda1cab966b1ead38931844cda61
Larry Botha <larry@fixate.it>
2020-03-28 17:20:45 -0500

    add examples for validatoin

---
+++ section-08/89-validating-forms-and-inputs/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { isValidEmail } from "./helpers";
+
+  let email = "";
+  let formIsValid = false;
+
+  $: if (isValidEmail(email)) {
+    formIsValid = true;
+  } else {
+    formIsValid = false;
+  }
+</script>
+
+<style>
+  .valid {
+    border-color: green;
+  }
+
+  .invalid {
+    border-color: red;
+  }
+</style>
+
+<form on:submit|preventDefault>
+  <input
+    type="email"
+    bind:value={email}
+    class={isValidEmail(email) ? 'valid' : 'invalid'} />
+
+  <button type="submit" disabled={!formIsValid}>submit</button>
+</form>
