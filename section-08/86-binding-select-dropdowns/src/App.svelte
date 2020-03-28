40d53e7c98f77c72a928854d38e046109c8d100c
Larry Botha <larry@fixate.it>
2020-03-28 15:05:36 -0500

    add example binding to select element's value

---
+++ section-08/86-binding-select-dropdowns/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let selectedColor = "red";
+</script>
+
+<p>Selected color: {selectedColor}</p>
+
+<!--
+  In the same manner as binding to text inputs, one binds to the value property
+  on a select element
+
+  One could also set the value of each option to an object
+-->
+<select bind:value={selectedColor}>
+  <option value="red">red</option>
+  <option value="green">green</option>
+  <option value="blue">blue</option>
+</select>
