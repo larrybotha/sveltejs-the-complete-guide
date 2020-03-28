c4b4c25450e505b569d58efc2cc8253254128258
Larry Botha <larry@fixate.it>
2020-03-28 15:39:01 -0500

    add component reference binding example

---
+++ section-08/88-binding-to-component-references/src/toggle.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let current = 0;
+  export let ariaHidden = false;
+</script>
+
+<button
+  aria-hidden={ariaHidden}
+  disabled={current === 0}
+  on:click={() => (current = 0)}>
+  0
+</button>
+
+<button
+  aria-hidden={ariaHidden}
+  disabled={current === 1}
+  on:click={() => (current = 1)}>
+  1
+</button>
