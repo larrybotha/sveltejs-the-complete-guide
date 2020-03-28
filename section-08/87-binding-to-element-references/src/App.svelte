8c5bf4ca3400196da2df5a924e5bafbc62e96771
Larry Botha <larry@fixate.it>
2020-03-28 15:22:41 -0500

    add example of element binding

---
+++ section-08/87-binding-to-element-references/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  let inputRef;
+  let title;
+
+  function handleClick() {
+    /**
+     * We only read from the value of the input via the reference to the input
+     */
+    title = inputRef.value;
+
+    /**
+     * Print the value of the input as an object
+     */
+    console.dir(inputRef);
+  }
+</script>
+
+<p>title: {title}</p>
+
+<!--
+  Bind this input to a variable in this component.
+
+  We'll have a reference to an actual dom node
+-->
+<input type="text" bind:this={inputRef} />
+
+<button on:click={handleClick}>set text</button>
