ba823c649a305247e54db2d38cb6a34851425367
Larry Botha <larry@fixate.it>
2020-03-29 16:45:13 -0500

    add example of updating store data

---
+++ section-10/103-updating-store-data/src/Products/Product.svelte
@@ -0,0 +1,58 @@
+<script>
+  import { cartStore } from "../Cart/cart-store";
+  import Button from "../UI/Button.svelte";
+
+  export let id;
+  export let title;
+  export let price;
+  export let description;
+
+  function addToCart() {
+    /**
+     * Update the existing items in the  store using store.update
+     */
+    cartStore.update(items => {
+      return [...items, { id, title, price }];
+    });
+  }
+</script>
+
+<style>
+  .product {
+    background: white;
+    border-radius: 5px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    margin: 1rem 0;
+  }
+
+  h1 {
+    font-size: 1.25rem;
+    font-family: "Roboto Slab", serif;
+    margin: 0;
+  }
+
+  h2 {
+    font-size: 1rem;
+    margin: 0;
+    color: #494949;
+  }
+
+  p {
+    margin: 0;
+  }
+</style>
+
+<div class="product">
+  <div>
+    <h1>{title}</h1>
+    <h2>{price}</h2>
+    <p>{description}</p>
+  </div>
+
+  <div>
+    <Button on:click={addToCart}>Add to Cart</Button>
+  </div>
+</div>
