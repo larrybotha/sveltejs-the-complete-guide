d900e6d1f1cb907f5b302692691392eea8e5ee26
Larry Botha <larry@fixate.it>
2020-03-29 19:43:15 -0500

    add example for short-lived subscriptions

---
+++ section-10/108-subscribing-for-a-short-period/src/Cart/Cart.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { onDestroy } from "svelte";
+
+  import CartItem from "./CartItem.svelte";
+  import { cartStore } from "./cart-store.js";
+</script>
+
+<style>
+  section {
+    width: 30rem;
+    max-width: 90%;
+    margin: 2rem auto;
+    border-bottom: 2px solid #ccc;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+</style>
+
+<section>
+  <h1>Cart</h1>
+
+  <ul>
+    <!--
+      We can automatically subscribe to the value in the store by prepending the
+      store with $
+    -->
+    {#each $cartStore as item (item.id)}
+      <CartItem id={item.id} title={item.title} price={item.price} />
+    {:else}
+      <p>No items in cart yet!</p>
+    {/each}
+  </ul>
+</section>
---
+++ section-10/108-subscribing-for-a-short-period/src/Products/Products.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { productStore } from "./product-store.js";
+  import Product from "../Products/Product.svelte";
+</script>
+
+<style>
+  section {
+    width: 30rem;
+    max-width: 90%;
+    margin: 2rem auto;
+  }
+</style>
+
+<section>
+  <h1>Products</h1>
+  {#each $productStore as product (product.id)}
+    <Product
+      id={product.id}
+      title={product.title}
+      price={product.price}
+      description={product.description} />
+  {/each}
+</section>
