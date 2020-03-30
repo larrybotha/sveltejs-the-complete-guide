5f4c224cb01bd1fb77cf6124e4513c3dd671661f
Larry Botha <larry@fixate.it>
2020-03-29 19:21:16 -0500

    add examples for adding a store

---
+++ section-10/107-a-second-store/src/Cart/CartItem.svelte
@@ -0,0 +1,52 @@
+<script>
+  import { cartStore } from "./cart-store.js";
+  import { productStore } from "../Products/product-store.js";
+  import Button from "../UI/Button.svelte";
+
+  export let title;
+  export let price;
+  export let id;
+
+  let showDescription = false;
+
+  function displayDescription() {
+    showDescription = !showDescription;
+  }
+
+  function removeFromCart() {
+    cartStore.update(items => items.filter(item => item.id !== id));
+  }
+</script>
+
+<style>
+  li {
+    margin: 1rem 0;
+    border-radius: 5px;
+    background: white;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
+  }
+
+  h1,
+  h2 {
+    font-size: 1rem;
+    margin: 0;
+  }
+
+  h2 {
+    color: #494949;
+    margin-bottom: 1rem;
+  }
+</style>
+
+<li>
+  <h1>{title}</h1>
+  <h2>{price}</h2>
+  <Button mode="outline" on:click={displayDescription}>
+    {showDescription ? 'Hide Description' : 'Show Description'}
+  </Button>
+  <Button on:click={removeFromCart}>Remove from Cart</Button>
+  {#if showDescription}
+    <p>Not available :(</p>
+  {/if}
+</li>
