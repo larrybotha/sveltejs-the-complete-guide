a92f3e7f87ea1e2226ea66de7e951a6f345e4a9f
Larry Botha <larry@fixate.it>
2020-04-09 23:12:36 -0500

    add example of recursive component

---
+++ section-16/165-recursive-components/src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+  import RecursiveComponent from "./recursive-component.svelte";
+</script>
+
+<RecursiveComponent />
---
+++ section-16/165-recursive-components/src/recursive-component.svelte
@@ -0,0 +1,24 @@
+<script>
+  let items = [];
+
+  function add() {
+    items = items.concat(Date.now());
+  }
+</script>
+
+<style>
+  div {
+    margin-left: 1em;
+  }
+</style>
+
+<button on:click={add}>add child</button>
+
+<div>
+  {#each items as item}
+    item added: {item}
+    <div>
+      <svelte:self />
+    </div>
+  {/each}
+</div>
