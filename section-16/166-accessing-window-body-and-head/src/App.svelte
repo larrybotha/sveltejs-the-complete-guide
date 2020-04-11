18aad22874fde8b7c0779adf6a0e87e5ca9aa077
Larry Botha <larry@fixate.it>
2020-04-10 23:26:42 -0500

    add window, body, and head examples

---
+++ section-16/166-accessing-window-body-and-head/src/App.svelte
@@ -0,0 +1,81 @@
+<script>
+  let dims;
+  let mouseCoords;
+  let title = "default title";
+  let description = "description";
+  let pageYOffset = 0;
+  let offsetHeight;
+  $: scrollPercentage = (pageYOffset / offsetHeight) * 100;
+
+  function handleResize() {
+    const { innerWidth, innerHeight } = window;
+
+    dims = { innerWidth, innerHeight };
+  }
+
+  function handleMouseMove(ev) {
+    const { clientX, clientY } = ev;
+
+    mouseCoords = { clientX, clientY };
+  }
+</script>
+
+<style>
+  div {
+    min-height: 120vh;
+  }
+</style>
+
+<!--
+  Window-level events could be managed inside Svelte's onMount lifecycle hook,
+  but Svelte provides an internal <svelte:window /> component where events can
+  be bound
+-->
+<svelte:window
+  on:DOMContentLoaded={() => {
+    handleResize();
+    console.log('DOMContentLoaded');
+    offsetHeight = document.body.offsetHeight;
+  }}
+  on:resize={handleResize}
+  bind:scrollY={pageYOffset} />
+
+<!--
+  Body-level events can be bound using Svelte's <svelte:body /> internal
+  component
+-->
+<svelte:body on:mousemove={handleMouseMove} />
+
+<!--
+  Svelte allows manipulation of content in the head of the document through its
+  <svelte:head /> internal component
+-->
+<svelte:head>
+  <title>{title}</title>
+
+</svelte:head>
+
+<div class="container">
+  <h1>Window dims</h1>
+
+  <pre>{JSON.stringify(dims, null, 2)}</pre>
+
+  <h1>Mouse coords</h1>
+
+  <pre>{JSON.stringify(mouseCoords, null, 2)}</pre>
+
+  <h1>Scroll percentage</h1>
+
+  <pre>{scrollPercentage}%</pre>
+
+  <h1>
+    Dynamic
+    <code>head</code>
+  </h1>
+
+  <label>
+    title
+    <br />
+    <input type="text" bind:value={title} />
+  </label>
+</div>
